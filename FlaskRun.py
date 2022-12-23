5899985b8168cc7bf35255d00b0a7fed0bf5555f
aid12345 <aid12345@naver.com>
2022-12-23 22:26:56 +0900

    first upload

---
+++ FlaskRun.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'flask test~~~~ 이주애 화이팅~~'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=27017, debug=True)
---
+++ MongoDbConn.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sa:<sa1@#4>@cluster0.smcovev.mongodb.net/Cluster0?retryWrites=true&w=majority')
+
+db = client.Cluster0
